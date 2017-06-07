9d8c0c9e9c122f198ac091d0dff5193900dc04de
sosubramanian <lings24@gmail.com>
2017-06-07 16:57:43 -0400

    Init Commit

---
+++ src/main/java/com/project/handcricket/HandcricketApplication.java
@@ -0,0 +1,12 @@
+package com.project.handcricket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandcricketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandcricketApplication.class, args);
+	}
+}

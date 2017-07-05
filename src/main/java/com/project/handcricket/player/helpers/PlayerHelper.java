e1ea8f0549726fb88ab98d9ee18a98a05fb50be4
sosubramanian <lings24@gmail.com>
2017-07-05 10:13:16 -0400

    Player Disconnect Null Check

--- src/main/java/com/project/handcricket/player/helpers/PlayerHelper.java
+++ src/main/java/com/project/handcricket/player/helpers/PlayerHelper.java
@@ -9,6 +9,7 @@ public class PlayerHelper {
 
   public static Player getPlayer(String gameId, String playerId) {
     Game game = GameDB.getInstance().getGame(gameId);
+    if (game == null) return null;
     if (game.getBatsman().getId().equals(playerId))
       return game.getBatsman();
     else

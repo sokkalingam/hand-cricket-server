abfa1b8e2d52830e1877b5aa1d65083a44cdecf9
Sokkalingam Subramanian <sokkalingam.subramanian@zoominfo.com>
2019-08-12 11:56:48 -0400

    part_3_game_id: Helper

--- src/main/java/com/project/handcricket/helper/Helper.java
+++ src/main/java/com/project/handcricket/helper/Helper.java
@@ -7,7 +7,11 @@ import java.util.UUID;
 public class Helper {
 
   public static String getRandomID(int len) {
-    return RandomStringUtils.randomAlphanumeric(len).toUpperCase();
+    return RandomStringUtils.randomNumeric(len);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(getRandomID(6));
   }
 
 }
